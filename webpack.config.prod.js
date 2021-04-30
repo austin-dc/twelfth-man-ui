93b771d0499459e092df94d153b1ff9724ba26e0
William Blake <gwilliamblake@gmail.com>
2021-04-30 09:09:52 -0500

    Eject create react app (#3)
    Setup typescript and webpack configuration for development after ejecting app. This allows devs more control and flexibility with tooling and the chance to learn the build process.
    
    -eject create-react-app
    -ignore /dist files
    -update yarn.lock
    -update typescript configuration
    -add webpack configuration

---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = merge(commonConfig, {
+    mode: 'production',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new OptimizeCssAssetsPlugin({
+                cssProcessorOptions: {
+                    map: {
+                        inline: false,
+                        annotation: true
+                    }
+                }
+            }),
+            new TerserPlugin({
+                parallel: true
+            })
+        ]
+    }
+});
